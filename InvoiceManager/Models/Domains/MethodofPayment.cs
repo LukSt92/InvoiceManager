82d8c9238a7aa9bc01b701ceec217e3f7b77acda
LukSt92 <stach.stachu@gmail.com>
2022-05-04 19:49:58 +0200

    Add project files.

---
+++ InvoiceManager/Models/Domains/MethodofPayment.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace InvoiceManager.Models
+{
+    public class MethodofPayment
+    {
+        public MethodofPayment()
+        {
+            Invoices = new Collection<Invoice>();
+        }
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+
+        public ICollection<Invoice> Invoices { get; set; }
+    }
+}
